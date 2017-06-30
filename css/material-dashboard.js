5cd4a9bd89fd82dda446a97399579edd45820b74
Asith Wijenayake <asith.chamidu12@gmail.com>
2017-06-30 10:49:09 +0530

    Updates

--- css/material-dashboard.js
+++ css/material-dashboard.js
@@ -651,11 +651,11 @@ export default StyleSheet.create({
         "boxShadow": "0 12px 20px -10px rgba(244, 67, 54, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(244, 67, 54, 0.2)"
     },
     "sidebar[data-color=\"purple\"] nav liactive a": {
-        "backgroundColor": "#9c27b0",
+        "backgroundColor": "#2d6cdf",
         "boxShadow": "0 12px 20px -10px rgba(156, 39, 176, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(156, 39, 176, 0.2)"
     },
     "off-canvas-sidebar[data-color=\"purple\"] nav liactive a": {
-        "backgroundColor": "#9c27b0",
+        "backgroundColor": "#2d6cdf",
         "boxShadow": "0 12px 20px -10px rgba(156, 39, 176, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(156, 39, 176, 0.2)"
     },
     "off-canvas-sidebar nav > li > a": {
@@ -5998,32 +5998,32 @@ export default StyleSheet.create({
         "color": "#FFFFFF"
     },
     "navbar dropdown-menu li a:hover": {
-        "backgroundColor": "#9c27b0",
+        "backgroundColor": "#2d6cdf",
         "color": "#FFFFFF",
         "boxShadow": "0 12px 20px -10px rgba(156, 39, 176, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(156, 39, 176, 0.2)"
     },
     "navbar dropdown-menu li a:focus": {
-        "backgroundColor": "#9c27b0",
+        "backgroundColor": "#2d6cdf",
         "color": "#FFFFFF",
         "boxShadow": "0 12px 20px -10px rgba(156, 39, 176, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(156, 39, 176, 0.2)"
     },
     "navbar dropdown-menu li a:active": {
-        "backgroundColor": "#9c27b0",
+        "backgroundColor": "#2d6cdf",
         "color": "#FFFFFF",
         "boxShadow": "0 12px 20px -10px rgba(156, 39, 176, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(156, 39, 176, 0.2)"
     },
     "navbarnavbar-default dropdown-menu li a:hover": {
-        "backgroundColor": "#9c27b0",
+        "backgroundColor": "#2d6cdf",
         "color": "#FFFFFF",
         "boxShadow": "0 12px 20px -10px rgba(156, 39, 176, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(156, 39, 176, 0.2)"
     },
     "navbarnavbar-default dropdown-menu li a:focus": {
-        "backgroundColor": "#9c27b0",
+        "backgroundColor": "#2d6cdf",
         "color": "#FFFFFF",
         "boxShadow": "0 12px 20px -10px rgba(156, 39, 176, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(156, 39, 176, 0.2)"
     },
     "navbarnavbar-default dropdown-menu li a:active": {
-        "backgroundColor": "#9c27b0",
+        "backgroundColor": "#2d6cdf",
         "color": "#FFFFFF",
         "boxShadow": "0 12px 20px -10px rgba(156, 39, 176, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(156, 39, 176, 0.2)"
     },
